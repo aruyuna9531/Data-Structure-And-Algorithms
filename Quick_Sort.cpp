ef621887976c9dedbf19509cc8e69a97b116016d
yuna <33777205+aruyuna9531@users.noreply.github.com>
2018-08-05 12:44:42 +0800

    Create Quick_Sort.cpp

---
+++ Quick_Sort.cpp
@@ -0,0 +1,36 @@
+//快速排序
+void swap(int &i, int &j){
+        int tmp=i;
+        i=j;
+        j=tmp;
+}
+//普通数组快排
+template<typename T>
+void qsort(T *arr, int start, int end){
+        if(start>=end)return;
+        int sta=arr[start],i=start+1,j=end;
+        while(i<=j){
+            while(i<=end && arr[i]<=sta)i++;
+            while(j>start && arr[j]>=sta)j--;
+            if(i>j)break;
+            swap(arr[i],arr[j]);
+        }
+        swap(arr[start],arr[j]);
+        qsort(arr,start,j-1);
+        qsort(arr,j+1,end);
+}
+//容器vector快排
+template<typename T>
+void qsort(vector<T>& arr, int start, int end){
+        if(start>=end)return;
+        int sta=arr[start],i=start+1,j=end;
+        while(i<=j){
+            while(i<=end && arr[i]<=sta)i++;
+            while(j>start && arr[j]>=sta)j--;
+            if(i>j)break;
+            swap(arr[i],arr[j]);
+        }
+        swap(arr[start],arr[j]);
+        qsort(arr,start,j-1);
+        qsort(arr,j+1,end);
+}
